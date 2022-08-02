943c022ca65ffe54c6c0bcfcff8a9582415494a5
suryajayantara <ptsuryajayantara01@gmail.com>
2022-08-02 18:31:14 +0800

    :tada: adding comment

--- lib/helpers/regex_seting.dart
+++ lib/helpers/regex_seting.dart
@@ -5,6 +5,7 @@ class RegexTypeExp {
 
   
   */
+  // New Regex Added
   static final emailType = RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
   static final phoneType = RegExp(r"^\+?0[0-9]{10}$");
   static final nameRegExp =
