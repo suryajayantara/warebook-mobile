81eccd0f25dff2211d0c29a89d292e6aeab9e278
RisaRynanda <risarynanda1601@gmail.com>
2022-08-02 17:51:11 +0800

    :tada: card form is done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:warebook_mobile/views/menu_form_card.dart';
 import 'package:warebook_mobile/views/pages/thesis/create_repo_thesis.dart';
 
 void main(List<String> args) async {
@@ -11,14 +12,14 @@ void main(List<String> args) async {
 class MyApp extends StatelessWidget {
   MyApp({Key? key}) : super(key: key);
   final dataStorage = GetStorage('auth');
-  
+
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       // home: (dataStorage.read('token') == null) ? OnboardingView() : HomeView(),
-      home: CreatingThesisRepoView(),
+      home: FormCard(),
       debugShowCheckedModeBanner: false,
       theme: ThemeData(brightness: Brightness.light),
-    );  
+    );
   }
 }
